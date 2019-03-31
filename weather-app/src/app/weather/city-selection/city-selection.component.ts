0ad0390209f387850693d3ffaf443f55d09eb992
Filip <gruczka.f@gmail.com>
2019-03-31 23:02:22 +0200

    added city.type.ts + added city-selection component to weather component

--- weather-app/src/app/weather/city-selection/city-selection.component.ts
+++ weather-app/src/app/weather/city-selection/city-selection.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-city-selection',
   templateUrl: './city-selection.component.html',
-  styleUrls: ['./city-selection.component.css']
+  styleUrls: ['./city-selection.component.scss']
 })
 export class CitySelectionComponent implements OnInit {
 
