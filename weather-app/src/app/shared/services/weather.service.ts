fa06f6e1360912d8d1c888a5709809c26f87e4a1
Filip <gruczka.f@gmail.com>
2019-04-10 18:48:57 +0200

    added router.navigate() to navigate to weather component (passing longitude and langitude); implemented  destructuring of an object + base configuration of longitude and lanitude api - mapbox

--- weather-app/src/app/shared/services/weather.service.ts
+++ weather-app/src/app/shared/services/weather.service.ts
@@ -11,4 +11,8 @@ export class WeatherService {
   getWrather() {
     return this.http.get('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/985434461670fbd750ec0ed8883d74bc/37.8267,-122.4233');
   }
+
+  getLatitudeLongitude() {
+    return this.http.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZmlsaXBnIiwiYSI6ImNqdWJkemhkYjBjZzk0M21sc2RpYnlrZ2oifQ.kLlhqHGZUJKLCDk9x1iEbw');
+  }
 }
--- weather-app/src/app/weather/city-selection/city-selection.component.ts
+++ weather-app/src/app/weather/city-selection/city-selection.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CityService } from 'src/app/shared/services/city.service';
 import { City } from 'src/app/shared/city.type';
 import { WeatherService } from 'src/app/shared/services/weather.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-city-selection',
@@ -15,7 +16,8 @@ export class CitySelectionComponent implements OnInit {
   cities: City[] = [];
 
   constructor(private cityService: CityService,
-    private weatherService: WeatherService) { }
+    private weatherService: WeatherService,
+    private router: Router) { }
 
   ngOnInit() {
     this.cities = this.cityService.cities;
@@ -26,11 +28,22 @@ export class CitySelectionComponent implements OnInit {
 
   checkWeather(city: string) {
     if(this.selectedCity && city === this.selectedCity.city) {
-      console.log('We will use mocked objects - ', this.selectedCity.city, 
-      this.selectedCity.latitude, this.selectedCity.longitude);
+      const {city, latitude, longitude} = this.selectedCity;
+      console.log('We will use mocked objects - ', city, latitude, longitude);
+      this.router.navigate(['/weather-for-city', {latitude, longitude}]);
     } else {
-      console.log("I just have city string name - " + city);      
+      console.log("I just have city string name - " + city);
+      this.fetchLatitudeLongitude(city);
     }
+  }
+
+  fetchLatitudeLongitude(city: string) {
+    this.weatherService.getLatitudeLongitude().subscribe((data: any) => {
+      console.log('longitude - ' + data.features[0].center[0]);
+      console.log('latitude - ' + data.features[0].center[1]);
+    }, error => {
+      console.log('Error !!!!!! ' + error);
+    });
   }
 
   selectCity(city: City) {
