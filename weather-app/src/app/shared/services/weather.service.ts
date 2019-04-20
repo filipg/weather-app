3c56b7caddf6e19930037187ee9e3eeacd7ed715
Filip <gruczka.f@gmail.com>
2019-04-20 22:20:25 +0200

    exclude request option (daily, flags ...)

--- weather-app/src/app/shared/services/weather.service.ts
+++ weather-app/src/app/shared/services/weather.service.ts
@@ -21,7 +21,7 @@ export class WeatherService {
   }
 
   getWeather(latitude: string, longitude: string) {
-    return this.http.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/985434461670fbd750ec0ed8883d74bc/${latitude},${longitude}`);
+    return this.http.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/985434461670fbd750ec0ed8883d74bc/${latitude},${longitude}?exclude=minutely,hourly,daily,alerts,flags`);
   }
 
   getLatitudeLongitude(city: string) {
