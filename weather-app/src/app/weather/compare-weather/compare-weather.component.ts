b7bebc93bffc949d82fdd066dfee2d526b1a1ed3
Filip <gruczka.f@gmail.com>
2019-05-02 21:27:56 +0200

    added base layout + style for compare-weather component

--- weather-app/src/app/weather/compare-weather/compare-weather.component.ts
+++ weather-app/src/app/weather/compare-weather/compare-weather.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-compare-weather',
   templateUrl: './compare-weather.component.html',
-  styleUrls: ['./compare-weather.component.css']
+  styleUrls: ['./compare-weather.component.scss']
 })
 export class CompareWeatherComponent implements OnInit {
 
